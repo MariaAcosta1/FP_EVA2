83f3b5b5d90a13434e4734b68deb4e178c343d77
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2025-10-06 17:53:50 -0600

    Add files via upload

---
+++ EVA2_9_FOR/EVA2_9_FOR.java
@@ -0,0 +1,24 @@
+
+package eva2_9_for;
+
+
+public class EVA2_9_FOR {
+
+
+    public static void main(String[] args) {
+        //3 PARÁMETROS PARA FUNCIONAR
+        //PUNTO INICIAL 
+        //DONDE ACABA
+        //COMO ME VOY A MOVER
+        //i --> índice (arreglos)
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + "HOLA MUNDO!!!"); 
+            System.out.println();
+        }
+        for(int i = 10; i >= 1; i--){
+            System.out.println(i + "HOLA MUNDO!");
+            System.out.println();
+        }
+    }
+    
+}
